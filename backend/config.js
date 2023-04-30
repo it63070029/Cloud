c36e304c2572fc5de0608f54a6d46e89557509ce
Kotchakorn63070001 <63070001@kmitl.ac.th>
2023-04-30 23:42:40 +0700

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'POEexm73',
-  database: 'wp',
+  host: 'dbnovel.ctvy2er5p7yn.us-east-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'Pass1234',
+  database: 'DBNovel',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
