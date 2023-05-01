8ad6a5ad45acfed706b12b9272e0570df9f641a4
Kotchakorn63070001 <63070001@kmitl.ac.th>
2023-05-01 22:19:59 +0700

    Delete test.js

--- backend/routes/test.js
+++
@@ -1,7 +0,0 @@
-const { uploadFile } = require('../s3upload')
-// const bodyParser = require('body-parser');
-const cors=require('cors');
-const dotenv = require('dotenv')
-const app=express();
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended:true}));
